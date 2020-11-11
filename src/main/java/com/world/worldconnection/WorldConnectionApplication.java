333d8c519f6bfbb5546d72ec80d737629c1f6e7a
Matheus Araujo de Morais <matheus.morais@codata.pb.gov.br>
2020-11-11 15:38:41 -0300

    First Commit

---
+++ src/main/java/com/world/worldconnection/WorldConnectionApplication.java
@@ -0,0 +1,13 @@
+package com.world.worldconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldConnectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/world/worldconnection/WorldConnectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.world.worldconnection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldConnectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
